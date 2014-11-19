0a9924695863b89ef7f80bd90d9b237c53f5b116
stagas <gstagas@gmail.com>
2014-11-19 14:29:44 +0000

    part 01

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 
 /**
  * @name tutorial
- * @part 00-init
+ * @part 01-getting-started
  * @org wavepot
  * @license pd
+ * 
+ * Getting started
+ * ---------------
+ * 
+ * This is a live dsp editor.
+ * 
+ * Function `dsp` is invoked with the parameter `t` that
+ * represents the time in seconds since we pressed play.
+ * 
+ * Our job is to return sample values (range -1..1)
+ * to "draw" the audio waveform.
+ * 
+ * A waveform looks like this:
+ * 
+ *   vol
+ *    +1  dsp(t=0.1)
+ *     |     /\      /\
+ *    0|____/__\___ /__\__ t
+ *     |\  /    \  /
+ *     | \/      \/
+ *    -1      dsp(t=0.2)
+ * 
+ * Below is the most basic example producing
+ * a single sine wave oscillating at 440 Hz:
+ * 
+ * (note: it may be loud! lower your speakers!)
  */
 
 export function dsp(t) {
-  return 0;
+  return Math.sin(t * Math.PI * 2 * 440);
+}
+
+/**
+ * ### Exercise:
+ * 
+ * Complete the function to accept a frequency in Hz and return a sample:
+ */
+
+function sin(){
+  //
 }
