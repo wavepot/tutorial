17018ef4d25da2bcda53ce4adcbdb55e9b32ca49
stagas <gstagas@gmail.com>
2014-11-20 08:50:10 +0000

    part 02

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
 
 /**
- * test
+ * This is the test case (test.js) for
+ * the module on the left (index.js).
  */
 
+import sin from './index'; // import the default fn
+import { noise } from './index'; // import just noise
+
+export function dsp(t) {
+  return (sin(t, 440) + noise()) * 0.1;
+}
